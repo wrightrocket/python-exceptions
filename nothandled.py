1a9c79994ab03a7bde6f3136d1f110f81f7389f2
wrightrocket <wright.keith@gmail.com>
2020-09-19 00:54:41 -0700

    First idea of using format

---
+++ nothandled.py
@@ -0,0 +1,18 @@
+'''
+Example of why exception handling may be needed.
+'''
+__author__ = 'Keith Wright'
+# Assign numerator by default to 1000
+numerator = 1000
+
+# Receive user_input for a number to use as denominator as a 'str'
+user_input = input('Number to divide 1000 by: ')
+
+# Convert user_input to a floating point value for the denominator
+denominator = float(user_input)
+
+# Calculuator result as numerator divided by denominator
+result = numerator / denominator
+
+# Display the calculation and the result
+print ('%f divided by %f equals %f'.format(numerator, denominator))

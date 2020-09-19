6d78ef06b87930bcb9df52564befd45923f58c0c
wrightrocket <wright.keith@gmail.com>
2020-09-19 14:44:34 -0700

    Detailed example of raising an exception

---
+++ raise_age.py
@@ -0,0 +1,37 @@
+''' Example of raising an exception where it might make sense 
+
+If you wanted to calculate 100.0 divided by a person's age, then a value larger than zero
+would make sense. This program will raise a ZeroDivisionError if the age input is less than
+or equal to zero.
+'''
+
+# Receive input from the user as a string for their age.
+
+age = input("\nHow many years old are you?  ")
+
+# Convert the age string variable into an integer.
+
+age = int(age)
+
+# Check for truth if the age entered is less than or equal to 0 (zero).
+
+if age <= 0:
+
+    # If it is true that the age is less than or equal to 0(zero), 
+    # raise the ZeroDivisionError which will cause the program to exit
+
+    raise ZeroDivisionError
+
+# If it false that the age <= 0, then print the age divided by 100.0.
+
+else:
+
+    # Calculate age quotient
+
+    ageq = 100 / age
+
+    # Print the ageq quotient value with 3 digits or spaces on the left and two 
+    # on the right of the decimal point.
+
+    print ('\nYour "100 age quotient" is: {: 3.2f}'.format(ageq))
+

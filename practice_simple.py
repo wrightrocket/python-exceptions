be7586a6af29eda2e63bae8f8a55f59ff8ea3ba9
wrightrocket <wright.keith@gmail.com>
2020-09-19 14:18:12 -0700

    Added an example to match Hands On Practice

--- practice_simple.py
+++ practice_simple.py
@@ -4,9 +4,17 @@
 
 colors = ["red", "blue", "green"]
 
+# Add error handling by using try block of code
 
+try:
+    # Try to print the sixth element in the colors list
 
-# print the sixth element in the colors list
+    print(colors[5])
 
-print(colors[5])
+# Catch the IndexError to handle it as the known exception
 
+except IndexError:
+
+    # Print an error message
+
+    print("You can't print the sixth element in a three element list!")

d0924083e3d07cee0aa75ad92d10445152d8d4af
wrightrocket <wright.keith@gmail.com>
2020-09-19 14:06:39 -0700

    No error handling

---
+++ practice_simple.py
@@ -0,0 +1,12 @@
+''' This demonstrates for practice how to do simple single exception handling '''
+
+# Assign a list of strings to the colors variable
+
+colors = ["red", "blue", "green"]
+
+
+
+# print the sixth element in the colors list
+
+print(colors)
+
